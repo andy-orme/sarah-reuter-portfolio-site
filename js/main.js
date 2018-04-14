a2a8900a86f36a9454505271ce69d9fde8fbf00c
Andy <andy@pixl8.co.uk>
2018-04-14 14:57:32 +0100

    Waypoint on homepage
    > Got a basic waypoint working on the image grid on homepage

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(function(){
 	$("#hamburger").on("click", function() {
 		$("nav").slideToggle();
 	});
-
 	// Stops the nav bugging if window is re-sized when toggled open
 	$(window).on("resize", function() {
 		if ($(window).width() >= 1024 && !$("nav").is(":visible")) {
@@ -14,6 +13,15 @@ $(function(){
 		}
 	});
 
+
+	// Waypoints
+	var imgGrid = $(".img-grid");
+
+	imgGrid.waypoint(function(){
+		imgGrid.addClass("js-img-grid-animate")
+	}, {offset:"50%"});
+
+
 	// Slick carousel function for when the screen is less than 768px
 	function checkWindowWidth() {
 		if ($(window).width() < 768) {
@@ -22,10 +30,8 @@ $(function(){
 			$('.img-grid').slick('unslick');
 		}
 	}
-
 	// Check window width on load
 	checkWindowWidth();
-
 	// Check window width on resize
 	$(window).on('resize', function() {
 		checkWindowWidth();
